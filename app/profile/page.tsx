974862a177ae68875988c07e00478155cc19ef04
Alydiira <z5419495@ad.unsw.edu.au>
2024-03-02 22:44:27 +1100

    added profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from 'react';
+// import { useRouter } from "next/router";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+// WORKING ON PROFILE FRONT END
+export default function Profile() {
+    // const router = useRouter();
+    return (
+    <div className="h-[100vh]">
+      <div className="h-[58.8%]">
+      <div className="grid grid-cols-2 gap-4 place-content-center h-48 ...">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+            <div>04</div>
+        </div>
+            
+        </div>
+    </div>
+        
+      )
+}
+
+
