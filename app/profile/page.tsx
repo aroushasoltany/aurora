ec0b1bed4e777c666cf5bc7c0d62d8bea5afff92
Alydiira <z5419495@ad.unsw.edu.au>
2024-03-03 10:54:00 +1100

    remade profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,79 +12,76 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import parentAvatar from "@/public/parent-avatar.png";
 import Image from "next/image";
+import logoBlack from "@/public/logos/aurora-black.png";
 
-// background
-// div className="bg-blue-200 h-screen" style={{
-//   backgroundImage: "url(/unihack.jpg)"
-// }}
-
-// TODO
-  // connect with mongo database to get user profike info
-  // change avatar pic
 export default function Profile() {
     const router = useRouter();
-  return (
-    <div>
-    <div className="bg-blue-200">
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex-1">
-        <div className="h-64 flex justify-center items-center mx-auto">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="Parent User"/>
-              <AvatarFallback>Parent-Avatar</AvatarFallback>
-            </Avatar>
+    return (
+        <div className="grid h-screen grid-cols-2">
+            <div className="bg-blue-200">
+                <div className="p-20 flex justify-center items-center">
+                        <Image
+                        src={logoBlack}
+                        alt="Aurora logo"
+                        width={200}
+                        height={200}
+                        />
+                </div>
+                <div>
+                    <p className="font-semibold text-center text-2xl">
+                        Welcome [User]!
+                    </p>
+                </div>
             </div>
+
+        <div className="flex justify-center items-center">
+        <Table>
+            <TableCaption>[User's] Aurora Information</TableCaption>
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-[100px]">Email</TableHead>
+                <TableCell></TableCell>
+                <TableHead></TableHead>
+                <TableCell className="text-right">...@gmail.com</TableCell>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                <TableHead className="font-medium">Username</TableHead>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell className="text-right">AllyP</TableCell>
+                </TableRow>
+            </TableBody>
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-[100px]">Password</TableHead>
+                <TableCell></TableCell>
+                <TableHead></TableHead>
+                <TableCell className="text-right">xxxx</TableCell>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                <TableHead className="font-medium">Family</TableHead>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell className="text-right">Sarah</TableCell>
+                </TableRow>
+            </TableBody>
+            <TableHeader>
+                <TableRow>
+                <TableHead className="w-[100px]">Library</TableHead>
+                <TableCell></TableCell>
+                <TableHead></TableHead>
+                <TableCell className="text-right">Book1</TableCell>
+                </TableRow>
+            </TableHeader>
+            </Table>
+
         </div>
-      </div>
-    
-    </div>
-      
-    <div className="bg-blue-200 flex justify-center mx-auto mb-20" >
-      <p className="font-sans text-3xl">Hi [User]!</p>
-    </div>
-
-    <div>
-      <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Email</TableHead>
-              <TableCell>...@gmail.com</TableCell>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            <TableRow>
-              <TableHead className="font-medium">Username</TableHead>
-              <TableCell>123AllyP</TableCell>
-            </TableRow>
-          </TableBody>
-
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Password</TableHead>
-              <TableCell>********</TableCell>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            <TableRow>
-              <TableHead className="font-medium">Children</TableHead>
-              <TableCell>Name1, Name2</TableCell>
-            </TableRow>
-          </TableBody>
-
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Books</TableHead>
-              <TableCell>title1, title2</TableCell>
-            </TableRow>
-          </TableHeader>
-      </Table>
-    </div>
-    </div>
-  )
+            
+        </div>
+    )
 }
-
-
