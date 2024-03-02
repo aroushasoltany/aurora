6a46aca4de024505ee98abaf37032125c5dd96ef
Alydiira <z5419495@ad.unsw.edu.au>
2024-03-03 00:08:42 +1100

    implemented basic user profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,26 +1,83 @@
 "use client"
 
 import React from 'react';
-// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
 
 // WORKING ON PROFILE FRONT END
 export default function Profile() {
-    // const router = useRouter();
-    return (
-    <div className="h-[100vh]">
-      <div className="h-[58.8%]">
-      <div className="grid grid-cols-2 gap-4 place-content-center h-48 ...">
-            <div>01</div>
-            <div>02</div>
-            <div>03</div>
-            <div>04</div>
+    const router = useRouter();
+  return (
+    <div className="bg-blue-200 h-screen">
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex-1">
+        <div className="h-64 flex justify-center items-center mx-auto">
+            <Avatar>
+              <AvatarImage src="https://png.pngtree.com/png-clipart/20201029/ourmid/pngtree-circle-clipart-blue-circle-png-image_2381949.jpg" alt="@shadcn"/>
+              <AvatarFallback>Parent-Avatar</AvatarFallback>
+            </Avatar>
+            </div>
         </div>
-            
-        </div>
+      </div>
     </div>
-        
-      )
+      
+    <div className="flex justify-center mx-auto mb-20" >
+      <p>[User's] Information</p>
+    </div>
+
+    <div>
+      <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Email</TableHead>
+              <TableHead>...@gmail.com</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">Username</TableCell>
+              <TableCell>123AllyP</TableCell>
+            </TableRow>
+          </TableBody>
+
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Password</TableHead>
+              <TableHead>********</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">Children</TableCell>
+              <TableCell>Name1, Name2</TableCell>
+            </TableRow>
+          </TableBody>
+
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Books</TableHead>
+              <TableHead>title1, title2</TableHead>
+            </TableRow>
+          </TableHeader>
+      </Table>
+    </div>
+
+    </div>
+  )
 }
 
 
