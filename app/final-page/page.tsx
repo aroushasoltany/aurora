0bf01d77a82364be4c64c738b1249a8ca3ee8d8b
Alydiira <z5419495@ad.unsw.edu.au>
2024-03-03 14:23:24 +1100

    update ui final page

--- app/final-page/page.tsx
+++ app/final-page/page.tsx
@@ -15,51 +15,66 @@ import {
   } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
 import Image from "next/image";
-import logoBlack from "@/public/logos/aurora-black.png";
+import dino from "@/public/dino.png";
+import bin from "@/public/bin.png";
+import heart from "@/public/heart.png";
 
 export default function Finalpage() {
 const router = useRouter();
   return (
-    <div className="grid h-screen grid-rows-2">
-        <div className=""></div>
-        <div className="bg-[#FDD2F5]">
+    
+<div className="grid h-screen grid-rows-2">
+        
+        <div className="flex justify-center items-center">
+                        <Image
+                        src={dino}
+                        alt="Aurora logo"
+                        width={300}
+                        height={300}
+                        />
+        </div>
+        
+        <div className="bg-orange-200">
 
-            <div className="grid h-full grid-cols-3">
+            <div className="grid h-full grid-cols-2">
+                
                 <div className="grid h-full grid-rows-2">
                     <div className="flex justify-center items-center">
                         <Image
-                        src={logoBlack}
+                        src={heart}
                         alt="Aurora logo"
-                        width={100}
-                        height={100}
+                        width={250}
+                        height={250}
                         />
                     </div>
                     <div className="flex justify-center items-center">
-                        <Button variant="outline">Read Again</Button>
+                        <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="outline">Save Story</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                            <AlertDialogTitle>Add your story?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                You can re-read saved stories in your library. You can delete this at any time.
+                            </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction>Continue</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                        </AlertDialog>
                     </div>
                 </div>
                 
                 <div className="grid h-full grid-rows-2">
                     <div className="flex justify-center items-center">
                         <Image
-                        src={logoBlack}
+                        src={bin}
                         alt="Aurora logo"
-                        width={100}
-                        height={100}
-                        />
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <Button variant="outline">Save Story</Button>
-                    </div>
-                </div>
-                
-                <div className="grid h-full grid-rows-2">
-                    <div className="flex justify-center items-center">
-                        <Image
-                        src={logoBlack}
-                        alt="Aurora logo"
-                        width={100}
-                        height={100}
+                        width={200}
+                        height={200}
                         />
                     </div>
                     <div className="flex justify-center items-center">
@@ -88,5 +103,11 @@ const router = useRouter();
 
         </div>
     </div>
+
+
+
+
+
+
   )
 }
